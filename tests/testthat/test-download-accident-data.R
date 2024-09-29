08f9c30e5e15585fa258097e725b92929f390728
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2024-09-29 18:43:21 +0900

    :sparkles: Use withr for creating temp directory

--- tests/testthat/test-download-accident-data.R
+++ tests/testthat/test-download-accident-data.R
@@ -1,4 +1,6 @@
 test_that("`download_accident_data` works with valid inputs", {
+  temp_dir <- withr::local_tempdir()
+
   local_mocked_bindings(download.file = function(url, destfile) {
     file.copy(mock_accident_data_path, destfile, overwrite = TRUE)
     return(destfile)
@@ -6,34 +8,39 @@ test_that("`download_accident_data` works with valid inputs", {
 
   # Test with "main" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("main", tempdir(), 2022)
+    download_accident_data("main", temp_dir, 2022)
   )
   expect_true(file.exists(downloaded_file_path))
   expect_match(downloaded_file_path, "honhyo_2022[.]csv$")
 
   # Test with "sub" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("sub", tempdir(), 2021)
+    download_accident_data("sub", temp_dir, 2021)
   )
   expect_true(file.exists(downloaded_file_path))
   expect_match(downloaded_file_path, "hojuhyo_2021[.]csv$")
 
   # Test with "highway" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("highway", tempdir(), 2020)
+    download_accident_data("highway", temp_dir, 2020)
   )
   expect_true(file.exists(downloaded_file_path))
+  cat(downloaded_file_path)
   expect_match(downloaded_file_path, "kosokuhyo_2020[.]csv$")
 })
 
 test_that("`download_accident_data` handles invalid data types", {
-  expect_error(download_accident_data("invalid", tempdir(), 2022),
+  temp_dir <- withr::local_tempdir()
+
+  expect_error(download_accident_data("invalid", temp_dir, 2022),
                "Invalid type. Please specify one of: main, sub, highway")
 })
 
 test_that("`download_accident_data` handles invalid years", {
-  expect_error(download_accident_data("main", tempdir(), 2018),
+  temp_dir <- withr::local_tempdir()
+
+  expect_error(download_accident_data("main", temp_dir, 2018),
                "The year must be between 2019 and 2023.")
-  expect_error(download_accident_data("main", tempdir(), 2024),
+  expect_error(download_accident_data("main", temp_dir, 2024),
                "The year must be between 2019 and 2023.")
 })
