2dd4e3ad3cec09b3f406a7a00c46318742be7321
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2024-09-29 19:41:45 +0900

    :white_check_mark: Add process creating temp dir

--- tests/testthat/setup-download-accident-data.R
+++ tests/testthat/setup-download-accident-data.R
@@ -1,1 +1,3 @@
 mock_accident_data_path <- test_path("mock-accident-data.csv")
+temp_dir <- file.path(tempdir(), "test-download-dir")
+dir.create(temp_dir, showWarnings = FALSE)
--- tests/testthat/test-download-accident-data.R
+++ tests/testthat/test-download-accident-data.R
@@ -6,12 +6,14 @@ test_that("`download_accident_data` works with valid inputs", {
 
   # Test with "main" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("main", tempdir(), 2022)
+    download_accident_data("main", temp_dir, 2022)
   )
 
   ## --DEBUG BEGIN--
-  cat("Tempdir: ", tempdir(), "\n")
+  cat("Tempdir: ", temp_dir, "\n")
   cat("Downloaded file path: ", downloaded_file_path, "\n")
+  cat("Is tempdir exists? ", dir.exists(temp_dir), "\n")
+  cat("Is downloaded file exists? ", file.exists(downloaded_file_path), "\n")
   ## --DEBUG END--
 
   expect_true(file.exists(downloaded_file_path))
@@ -19,14 +21,14 @@ test_that("`download_accident_data` works with valid inputs", {
 
   # Test with "sub" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("sub", tempdir(), 2021)
+    download_accident_data("sub", temp_dir, 2021)
   )
   expect_true(file.exists(downloaded_file_path))
   expect_match(downloaded_file_path, "hojuhyo_2021[.]csv$")
 
   # Test with "highway" data type
   downloaded_file_path <- suppressMessages(
-    download_accident_data("highway", tempdir(), 2020)
+    download_accident_data("highway", temp_dir, 2020)
   )
   expect_true(file.exists(downloaded_file_path))
   cat(downloaded_file_path)
@@ -34,13 +36,13 @@ test_that("`download_accident_data` works with valid inputs", {
 })
 
 test_that("`download_accident_data` handles invalid data types", {
-  expect_error(download_accident_data("invalid", tempdir(), 2022),
+  expect_error(download_accident_data("invalid", temp_dir, 2022),
                "Invalid type. Please specify one of: main, sub, highway")
 })
 
 test_that("`download_accident_data` handles invalid years", {
-  expect_error(download_accident_data("main", tempdir(), 2018),
+  expect_error(download_accident_data("main", temp_dir, 2018),
                "The year must be between 2019 and 2023.")
-  expect_error(download_accident_data("main", tempdir(), 2024),
+  expect_error(download_accident_data("main", temp_dir, 2024),
                "The year must be between 2019 and 2023.")
 })
