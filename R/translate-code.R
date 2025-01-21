d12b7046c21798986715734cc1d01c748dbcef0d
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-01-21 10:18:18 +0900

    :sparkles: Add `translate_code()` translate code into readble label

---
+++ R/translate-code.R
@@ -0,0 +1,55 @@
+#' Translate codes to human-readable labels using configuration data
+#'
+#' `translate_code()` translates codes to their corresponding human-readable
+#' labels based on the configuration data. It supports English and Japanese
+#' languages. If the code is not found in the configuration data, it will
+#' issue a warning and return `NA`.
+#'
+#' @param code A character vector of codes to be converted to labels.
+#' @param concept A string specifying the concept category
+#'   (e.g., "impact_part")
+#' @param lang A string specifying the output language. Default is "en".
+#'   Supported values are "en" (English) and "ja" (Japanese).
+#' @return A character vector of the same length as the input `codes`,
+#'   containing the corresponding labels. Invalid codes are converted to `NA`
+#'   with a warning.
+#' @export
+#' @examples
+#' # Convert a single code
+#' translate_code("1", "impact_part")
+#'
+#' # Convert multiple codes
+#' translate_code(c("1", "2"), "impact_part")
+#'
+#' # Convert codes with Japanese labels
+#' translate_code("1", "impact_part", "ja")
+#'
+#' # Handle invalid codes
+#' translate_code(c("1", "999"), "impact_part")
+#'
+translate_code <- function(code, concept, lang = "en") {
+  # Get configuration
+  config <- get_config()
+
+  # Extract the relevant concept configuration
+  concept_config <- config$concepts[[concept]]
+
+  # Create lookup table
+  lookup <- setNames(
+    sapply(concept_config, function(x) x$names[[lang]], USE.NAMES = FALSE),
+    sapply(concept_config, function(x) x$code, USE.NAMES = FALSE)
+  )
+
+  # Issue a warning if there are invalid codes
+  invalid_code <- code[!code %in% names(lookup)]
+  if (0 < length(invalid_code)) {
+    cli_alert_warning(
+      "In concept {concept}, The following code(s) is not registered: {paste(invalid_code, collapse = ', ')}"
+    )
+  }
+
+  # Map codes to their labels; assign NA if code is not found
+  label <- ifelse(code %in% names(lookup), lookup[code], NA_character_)
+
+  return(label)
+}
