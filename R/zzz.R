88ef016faab845dd05124024e6cc67f8fa4d7ad5
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-01-23 18:26:50 +0900

    :recycle: Remove irrelevant message

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,6 @@
 .onLoad <- function(libname, pkgname) {
-  tryCatch({
-    reset_config()
-  }, error = function(e) {
-    warning(
-      "Configuration file could not be loaded. ",
-      "Please check the config path."
-    )
-  })
+  # Reset settings to default
+  reset_config()
 
   # To disable the progress bar during data loading
   options(readr.show_progress = FALSE)
